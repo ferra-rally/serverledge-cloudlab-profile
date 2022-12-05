b4c232ca3687e1e9226c50a1537e19f54033da0c
Daniele Ferrarelli <danieleferrarelli@gmail.com>
2022-12-05 10:27:05 +0100

    Edited bridge

--- profile.py
+++ profile.py
@@ -76,7 +76,8 @@ for i in range(1, params.edgeNodes+1):
     node = request.XenVM(name)
     node.disk_image = params.osImage
     node.addService(pg.Execute(shell="bash", command="ip route add 192.168.2.0/24 via 192.168.1.1 dev eth0"))
-    nfsLan.addInterface(node.addInterface(pg.IPv4Address(f"192.168.1.{i+1}", "255.255.255.0")))
+    ip = f"192.168.1.{i+1}"
+    nfsLan.addInterface(node.addInterface(pg.IPv4Address(ip, "255.255.255.0")))
 
 for i in range(1, params.clientNodes+1):
     name = "edge" + str(i)
@@ -90,7 +91,8 @@ for i in range(1, params.cloudNodes + 1):
     node = request.RawPC(name)
     node.disk_image = params.osImage
     node.hardware_type = params.phystype
-    cloudLan.addInterface(node.addInterface(pg.IPv4Address(f"192.168.2.{i+1}", "255.255.255.0")))
+    ip = f"192.168.2.{i + 1}"
+    cloudLan.addInterface(node.addInterface(pg.IPv4Address(ip, "255.255.255.0")))
 
 
 router = request.XenVM("router")
