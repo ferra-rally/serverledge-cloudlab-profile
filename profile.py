f9427444c2fd59be4b908099de521c1b27c27e15
Daniele Ferrarelli <danieleferrarelli@gmail.com>
2022-12-06 14:46:53 +0100

    Updated profile.py

--- profile.py
+++ profile.py
@@ -74,15 +74,22 @@ for i in range(1, params.edgeNodes + 1):
     node = request.XenVM(name)
     node.disk_image = params.osImage
 
+
+    node.addService(rspec.Install(url="https://go.dev/dl/go1.19.3.linux-amd64.tar.gz", path="/usr/local"))
+
+    node.addService(rspec.Execute(shell="bash", command="wget https://raw.githubusercontent.com/ferra-rally"
+                                                        "/serverledge-cloudlab-profile/main/startup.sh"))
+    node.addService(rspec.Execute(shell="bash", command="startup.sh"))
+
+    """
     node.addService(rspec.Execute(shell="bash", command="wget https://bootstrap.pypa.io/get-pip.py; python3 "
                                                         "get-pip.py; python3 -m pip install --user ansible"))
-    node.addService(rspec.Install(url="https://go.dev/dl/go1.19.3.linux-amd64.tar.gz", path="/usr/local"))
     node.addService(rspec.Execute(shell="bash", command="sudo apt-get update; sudo apt-get install -y ca-certificates "
                                                         'curl gnupg lsb-release; echo "export '
                                                         'PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile'))
     node.addService(rspec.Execute(shell="bash", command="curl -fsSL https://get.docker.com -o get-docker.sh; sudo sh "
                                                         "get-docker.sh; sudo usermod -aG docker $USER; newgrp docker"))
-
+    """
     node.addService(rspec.Execute(shell="bash", command="ip route add 10.10.2.0/24 via 10.10.1.1 dev eth1"))
     ip = "10.10.1." + str(i + 1)
     interface = node.addInterface()
@@ -93,14 +100,22 @@ for i in range(1, params.clientNodes + 1):
     name = "client" + str(i)
     node = request.XenVM(name)
     node.disk_image = params.osImage
+
+    node.addService(rspec.Install(url="https://go.dev/dl/go1.19.3.linux-amd64.tar.gz", path="/usr/local"))
+
+    node.addService(rspec.Execute(shell="bash", command="wget https://raw.githubusercontent.com/ferra-rally"
+                                                        "/serverledge-cloudlab-profile/main/startup.sh"))
+    node.addService(rspec.Execute(shell="bash", command="startup.sh"))
+
+    """
     node.addService(rspec.Execute(shell="bash", command="wget https://bootstrap.pypa.io/get-pip.py; python3 "
                                                         "get-pip.py; python3 -m pip install --user ansible"))
-    node.addService(rspec.Install(url="https://go.dev/dl/go1.19.3.linux-amd64.tar.gz", path="/usr/local"))
     node.addService(rspec.Execute(shell="bash", command="sudo apt-get update; sudo apt-get install -y ca-certificates "
                                                         'curl gnupg lsb-release; echo "export '
                                                         'PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile'))
     node.addService(rspec.Execute(shell="bash", command="curl -fsSL https://get.docker.com -o get-docker.sh; sudo sh "
                                                         "get-docker.sh; sudo usermod -aG docker $USER; newgrp docker"))
+    """
 
     ip = "10.10.1." + str(100 + i)
     interface = node.addInterface()
@@ -117,17 +132,26 @@ for i in range(1, params.cloudNodes + 1):
 
     node.disk_image = params.osImage
 
-    node.addService(rspec.Execute(shell="bash", command="wget https://bootstrap.pypa.io/get-pip.py; python3 "
-                                                        "get-pip.py; python3 -m pip install --user ansible"))
+
     node.addService(rspec.Install(url="https://go.dev/dl/go1.19.3.linux-amd64.tar.gz", path="/usr/local"))
 
     node.addService(rspec.Execute(shell="bash", command="ip route add 10.10.1.0/24 via 10.10.2.1 dev eth1"))
+
+    node.addService(rspec.Execute(shell="bash", command="ip route add 10.10.1.0/24 via 10.10.2.1 dev eth1"))
+
+    node.addService(rspec.Execute(shell="bash", command="wget https://raw.githubusercontent.com/ferra-rally"
+                                                        "/serverledge-cloudlab-profile/main/startup.sh"))
+    node.addService(rspec.Execute(shell="bash", command="startup.sh"))
+
+    """
+    node.addService(rspec.Execute(shell="bash", command="wget https://bootstrap.pypa.io/get-pip.py; python3 "
+                                                        "get-pip.py; python3 -m pip install --user ansible"))
     node.addService(rspec.Execute(shell="bash", command="sudo apt-get update; sudo apt-get install -y ca-certificates "
                                                         'curl gnupg lsb-release; echo "export '
                                                         'PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile'))
     node.addService(rspec.Execute(shell="bash", command="curl -fsSL https://get.docker.com -o get-docker.sh; sudo sh "
                                                         "get-docker.sh; sudo usermod -aG docker $USER; newgrp docker"))
-
+    """
     ip = "10.10.2." + str(i + 1)
     interface = node.addInterface()
     interface.addAddress(rspec.IPv4Address(ip, "255.255.255.0"))
--- startup.sh
+++ startup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+python3 -m pip install --user ansible
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+echo "export PATH=$PATH:/usr/local/go/bin" >> "$HOME"/.profile
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker $USER
