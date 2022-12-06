11ab58d222d772331ac89c78c3e5e0663ef3295f
Daniele Ferrarelli <danieleferrarelli@gmail.com>
2022-12-06 16:22:17 +0100

    Added configuration files

--- get-repo.sh
+++ get-repo.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 
 git clone https://"$1"@github.com/ferra-rally/serverledge.git
+cd serverledge
+wget https://raw.githubusercontent.com/ferra-rally/serverledge-cloudlab-profile/main/config.yaml
+wget https://raw.githubusercontent.com/ferra-rally/serverledge-cloudlab-profile/main/config_cloud.yaml
+wget https://raw.githubusercontent.com/ferra-rally/serverledge-cloudlab-profile/main/classes.json
