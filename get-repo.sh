13c28090a72f2f9b64e474682f52a2a566434d09
Daniele Ferrarelli <danieleferrarelli@gmail.com>
2022-12-07 09:56:29 +0100

    Edited scripts

--- get-repo.sh
+++ get-repo.sh
@@ -5,3 +5,5 @@ cd serverledge
 wget https://raw.githubusercontent.com/ferra-rally/serverledge-cloudlab-profile/main/config.yaml
 wget https://raw.githubusercontent.com/ferra-rally/serverledge-cloudlab-profile/main/config_cloud.yaml
 wget https://raw.githubusercontent.com/ferra-rally/serverledge-cloudlab-profile/main/classes.json
+PATH=$PATH:/usr/local/go/bin make
+PATH=$PATH:/usr/local/go/bin bash
--- startup.sh
+++ startup.sh
@@ -10,7 +10,8 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker $USER
 newgrp docker
-wget https://bootstrap.pypa.io/get-pip.py -P /
+wget https://bootstrap.pypa.io/get-pip.py
+wget  https://raw.githubusercontent.com/ferra-rally/serverledge-cloudlab-profile/main/get-repo.sh -P /
 python3 get-pip.py
 python3 -m pip install --user ansible
 PATH=$PATH:/usr/local/go/bin bash
