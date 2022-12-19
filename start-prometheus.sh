b596a3f3f443db11ac74bece3cc1122dff9167ed
Daniele Ferrarelli <danieleferrarelli@gmail.com>
2022-12-19 14:46:58 +0100

    Edited prometheus start file

--- start-prometheus.sh
+++ start-prometheus.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 # Start Prometheus
-sudo prometheus --config.file=prometheus.yaml --web.listen-address=:8080 --web.enable-admin-api --storage.tsdb.path=./data
+sudo prometheus --config.file=prometheus.yaml --web.listen-address=:8080 --web.enable-admin-api --storage.tsdb.path=./data &
