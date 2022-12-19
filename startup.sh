9dd6d9fe1a51cf6a0b549a407962ebe539987427
Daniele Ferrarelli <danieleferrarelli@gmail.com>
2022-12-19 16:53:19 +0100

    Added proxy for influxdb on the edge

--- startup.sh
+++ startup.sh
@@ -4,12 +4,14 @@ wget https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
 python3 -m pip install --user ansible
 sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg lsb-release prometheus
+sudo apt-get install -y ca-certificates curl gnupg lsb-release prometheus nginx
 echo "export PATH=$PATH:/usr/local/go/bin" >> "$HOME"/.profile
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker $USER
 newgrp docker
 wget  https://raw.githubusercontent.com/ferra-rally/serverledge-cloudlab-profile/main/get-repo.sh -P $HOME
+sudo wget  https://raw.githubusercontent.com/ferra-rally/serverledge-cloudlab-profile/main/nginx.conf -P /etc/nginx/nginx.conf
+sudo systemctl start nginx
 setenv PATH ${PATH}:"/usr/local/go/bin"
 bash
