a06eb400680b4f67ad0d837e54a6109db1d4a167
Daniele Ferrarelli <danieleferrarelli@gmail.com>
2022-12-19 14:42:30 +0100

    Added prometheus config

--- get-repo.sh
+++ get-repo.sh
@@ -5,5 +5,8 @@ cd serverledge
 wget https://raw.githubusercontent.com/ferra-rally/serverledge-cloudlab-profile/main/config.yaml
 wget https://raw.githubusercontent.com/ferra-rally/serverledge-cloudlab-profile/main/config_cloud.yaml
 wget https://raw.githubusercontent.com/ferra-rally/serverledge-cloudlab-profile/main/classes.json
+wget https://raw.githubusercontent.com/ferra-rally/serverledge-cloudlab-profile/main/start-prometheus.sh
+wget https://raw.githubusercontent.com/ferra-rally/serverledge-cloudlab-profile/main/stop-prometheus.sh
+wget https://raw.githubusercontent.com/ferra-rally/serverledge-cloudlab-profile/main/prometheus.yaml
 PATH=$PATH:/usr/local/go/bin make
 PATH=$PATH:/usr/local/go/bin bash
---
+++ stop-prometheus.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+# Stop prometheus
+sudo killall prometheus
